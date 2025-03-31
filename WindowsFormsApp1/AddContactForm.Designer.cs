c6be0c6ffb6d2e0e41f87e8977520f8895cc5ae7
Khank2104 <nnkhanh159@gmail.com>
2025-03-31 18:54:38 +0700

    thêm Form để thêm liên hệ mới

---
+++ WindowsFormsApp1/AddContactForm.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class AddContactForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnsave = new System.Windows.Forms.Button();
+            this.txtname = new System.Windows.Forms.TextBox();
+            this.txtphone = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(205, 104);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Họ tên";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(205, 146);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "SDT";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // btnsave
+            // 
+            this.btnsave.Location = new System.Drawing.Point(342, 208);
+            this.btnsave.Name = "btnsave";
+            this.btnsave.Size = new System.Drawing.Size(75, 23);
+            this.btnsave.TabIndex = 3;
+            this.btnsave.Text = "lưu";
+            this.btnsave.UseVisualStyleBackColor = true;
+            // 
+            // txtname
+            // 
+            this.txtname.Location = new System.Drawing.Point(297, 98);
+            this.txtname.Name = "txtname";
+            this.txtname.Size = new System.Drawing.Size(163, 22);
+            this.txtname.TabIndex = 4;
+            // 
+            // txtphone
+            // 
+            this.txtphone.Location = new System.Drawing.Point(297, 146);
+            this.txtphone.Name = "txtphone";
+            this.txtphone.Size = new System.Drawing.Size(163, 22);
+            this.txtphone.TabIndex = 5;
+            // 
+            // AddContactForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtphone);
+            this.Controls.Add(this.txtname);
+            this.Controls.Add(this.btnsave);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "AddContactForm";
+            this.Text = "AddContactForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnsave;
+        private System.Windows.Forms.TextBox txtname;
+        private System.Windows.Forms.TextBox txtphone;
+    }
+}
