9d49042e38fcd0b770cec4da04eb2e97d7b9c9e2
Khank2104 <nnkhanh159@gmail.com>
2025-03-31 21:18:46 +0700

    giải quyết xung đột khi xóa form

--- WindowsFormsApp1/mainform.cs
+++ WindowsFormsApp1/mainform.cs
@@ -17,10 +17,6 @@ namespace WindowsFormsApp1
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-          
-
-        }
+       
     }
 }
